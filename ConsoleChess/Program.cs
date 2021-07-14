24589d9fb97ced9dcc899690cdb64c3ca9240a2f
rafamarquesrmb <rafa.rmb@gmail.com>
2021-07-14 07:43:38 -0300

    Primeiros passos da movimentação

--- ConsoleChess/Program.cs
+++ ConsoleChess/Program.cs
@@ -8,18 +8,23 @@ namespace ConsoleChess
     {
         static void Main(string[] args)
         {
-            try { 
-                Tabuleiro tab = new Tabuleiro(8, 8);
-                tab.colocarPeca(new Torre(tab,Cor.Preta),new Posicao(0,0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
-                
-                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(7, 7));
-                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(6, 7));
-                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(5, 7));
+            try {
+                PartidaDeXadrez partida = new PartidaDeXadrez();
 
+                while (!partida.terminada)
+                {
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab);
 
-                Tela.imprimirTabuleiro(tab);
+                    Console.WriteLine();
+                    Console.Write("Digite a Posição de Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+
+                    Console.Write("Digite a Posição de Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+
+                    partida.executaMovimento(origem, destino);
+                }                               
             }
             catch(TabuleiroException e)
             {
