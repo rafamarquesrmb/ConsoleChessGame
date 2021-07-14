ecf37f7091e82a7b31157b9a0ba6d5deeb1b407e
rafamarquesrmb <rafa.rmb@gmail.com>
2021-07-14 05:31:07 -0300

    Classe Posicao criada

--- ConsoleChess/Program.cs
+++ ConsoleChess/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using tabuleiro;
 
 namespace ConsoleChess
 {
@@ -6,7 +7,11 @@ namespace ConsoleChess
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + P);
+
+            Console.ReadLine();
         }
     }
 }
